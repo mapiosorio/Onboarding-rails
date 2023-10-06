d9ad6e6010f34a06b44aaf79e0b6c787dba0e3ca
mapiosorio <mosorio@eagerworks.com>
2023-10-06 15:12:52 -0300

    initial configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get '/users/sign_in', to: 'devise/sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
