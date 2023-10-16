473ae71bad795e386b6ad34eae33e3be9ed2d6e5
mapiosorio <142921351+mapiosorio@users.noreply.github.com>
2023-10-16 15:21:07 -0300

    User registration (#2)
    * Register as user
    
    * Fix style
    
    * Fix style and validate presence new fields
    
    * fix style

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module PlanIt
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en] # List available locales
   end
 end
