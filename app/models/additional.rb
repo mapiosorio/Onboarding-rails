eeecbc02b98ce2e95e07827f98797898cfc30188
mapiosorio <mosorio@eagerworks.com>
2023-11-15 15:54:44 -0300

    delete ransackable attributes from additional

--- app/models/additional.rb
+++ app/models/additional.rb
@@ -1,13 +1,5 @@
 class Additional < ApplicationRecord
   has_and_belongs_to_many :products
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "description", "id", "price", "updated_at"]
-  end
-
-  def self.ransackable_associations(auth_object = nil)
-    ["products"]
-  end
-
   validates :price, :description, presence: true
 end
