b97e4dd6c423d3a316a068c8a15e17d9464f9ae3
mapiosorio <mosorio@eagerworks.com>
2023-12-04 13:46:59 -0300

    Fix style

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
-  # def index
-  #   @search_query = params[:search]
-  #   @products = @search_query.present? ? Product.where('name ILIKE ?', "%#{@search_query}%") : Product.all
-  #   @pagy, @products = pagy(@products)
-  # end
-
   def show
     @category = Category.find(params[:id])
     @products = @category.products
